c396ec22d9294edc74753fa99a41af49db686ee8
l3msh0 <L3msh0@gmail.com>
2021-10-24 14:16:36 +0900

    Dynamic Routesまでやった

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
 import Link from "next/link";
-import utilStyles from "../styles/utils.module.css";
+import Layout, { siteTitle } from "../components/layout";
 import { getSortedPostsData } from "../lib/posts";
+import utilStyles from "../styles/utils.module.css";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -31,11 +31,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString="{date}"></Date>
+              </small>
             </li>
           ))}
         </ul>
